a6a5d85e3592763f1b097a90ad04dd8f5d107325
Adamsmiracle <adamsmiracle0@gmail.com>
2026-09-03 10:15:35 +0000

    udpate

--- catalog-service/src/main/java/com/bookstore/catalog_service/ApplicationProperties.java
+++ catalog-service/src/main/java/com/bookstore/catalog_service/ApplicationProperties.java
@@ -1,5 +1,6 @@
 package com.bookstore.catalog_service;
 
+
 import jakarta.validation.constraints.Min;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
